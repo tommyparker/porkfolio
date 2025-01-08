01d543a1fde5908024f5093a1503c2335d842fe2
Tommy <me@tommyparker.co.uk>
2025-01-08 21:15:05 +0000

    Footer and info

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,11 @@ import tailwind from '@astrojs/tailwind';
 
 import preact from '@astrojs/preact';
 
+import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
+
 export default defineConfig({
-  // ...
+  plugins: [svgr()],
   integrations: [tailwind(), preact()],
+  
 });
