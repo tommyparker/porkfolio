bd4b25e920fb6d733702215082ff27b6eefad5e0
Tommy <me@tommyparker.co.uk>
2025-01-19 13:35:34 +0000

    News styling

--- src/content/config.ts
+++ src/content/config.ts
@@ -17,11 +17,11 @@ const postsCollection = defineCollection({
     title: z.string(),
     pubDate: z.date(),
     description: z.string(),
-    author: z.string(),
     image: z.object({
       url: image(),
       alt: z.string(),
     }),
+    tags: z.array(z.string()).optional(),
   }),
 });
 
