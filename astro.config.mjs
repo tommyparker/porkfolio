3856a2f876fdc1f00c09dd2794bd7001a5cf404b
Tommy <me@tommyparker.co.uk>
2025-01-22 21:50:18 +0000

    GSAP out. Motion in.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import preact from '@astrojs/preact';
+import react from '@astrojs/react';
 import mdx from '@astrojs/mdx';
-
-import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
   plugins: [svgr()],
-  integrations: [tailwind(), preact(), mdx({
+  integrations: [tailwind(), react(), mdx({
     components: {
       'img': 'src/components/MdxImage.astro',
     }
   })],
-  
 });
--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -1,11 +1,11 @@
-import { useState } from 'preact/hooks';
+import { useState } from 'react';
 
-export default function Greeting({ messages, class: className = '' }) {
+export default function Greeting({ messages, className = '' }) {
   const randomMessage = () => messages[Math.floor(Math.random() * messages.length)];
 
   const [greeting] = useState(randomMessage());
 
   return (
-    <h1 class={`max-w-[1000px] ${className}`}>{greeting}! I’m&nbsp;Tommy&nbsp;Parker —&nbsp;a&nbsp;freelance illustrator.</h1>
+    <h1 className={`max-w-[1000px] ${className}`}>{greeting}! I’m&nbsp;Tommy&nbsp;Parker —&nbsp;a&nbsp;freelance illustrator.</h1>
   );
 }
