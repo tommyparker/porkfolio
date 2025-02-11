d9282f80d098316c8d5ed4782f8d1b48d72c4822
Tommy <me@tommyparker.co.uk>
2025-02-11 18:17:50 +0000

    RSS fix (3)

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,8 +1,6 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 import sanitizeHtml from 'sanitize-html';
-import MarkdownIt from 'markdown-it';
-const parser = new MarkdownIt();
 
 export async function GET(context) {
   const allPosts = await getCollection('posts');
@@ -10,14 +8,19 @@ export async function GET(context) {
     title: 'Drawn',
     description: 'A place for news from my studio, covering project and job updates, showing tidbits from behind the scenes, sharing inspiration from other places and people, and posting the occasional piece of writing.',
     site: context.site,
-    items: allPosts.map((post) => ({
-      title: post.data.title,
-      pubDate: post.data.pubDate,
-      description: post.data.description,
-      link: `/posts/${post.id}/`,
-      content: sanitizeHtml(parser.render(post.body), {
-        allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img'])
-      }),
-    })),
+    items: await Promise.all(
+      allPosts.map(async (post) => {
+        const { Content } = await post.render();
+        return {
+          title: post.data.title,
+          pubDate: post.data.pubDate,
+          description: post.data.description,
+          link: `/posts/${post.slug}/`,
+          content: sanitizeHtml(Content, {
+            allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img'])
+          }),
+        };
+      })
+    ),
   });
 }
