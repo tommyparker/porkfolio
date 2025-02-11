3b543e8a82a27ce065ca905cf0b21beeb62b177a
Tommy <me@tommyparker.co.uk>
2025-02-11 18:16:12 +0000

    RSS fix (2)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/static';
 import tailwind from '@astrojs/tailwind';
 import preact from '@astrojs/preact';
 import mdx from '@astrojs/mdx';
@@ -6,6 +7,14 @@ import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
   plugins: [svgr()],
+  vite: {
+    ssr: {
+      noExternal: ['sanitize-html'],
+      external: ['markdown-it']
+    }
+  },
+  output: 'static',
+  adapter: vercel(),
   integrations: [tailwind(), preact(), mdx({
     components: {
       'img': 'src/components/MdxImage.astro',
